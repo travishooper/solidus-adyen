b2d432a9771046f1699165fa0a9edabfabc3618e
Lou Giacalone <3@lgiacalone.com>
2018-05-10 17:56:48 -0700

    use gateway class

--- spec/models/spree/gateway/adyen_credit_card_spec.rb
+++ spec/models/spree/gateway/adyen_credit_card_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Spree::Gateway::AdyenCreditCard do
   it { is_expected.to be_a(Spree::Gateway) }
 
-  describe 'provider_class' do
-    subject { described_class.new.provider_class }
+  describe 'gateway_class' do
+    subject { described_class.new.gateway_class }
 
     it { is_expected.to eq(Adyen::REST) }
   end
